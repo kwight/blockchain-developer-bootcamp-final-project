a85bae582059c2f0de64e64b532ceebb3e06ae33
Kirk Wight <kwight@kwight.ca>
2021-10-16 13:39:34 -0700

    Allow the owner or a charity itself to remove a charity.
    This includes changing the `charities` variable to a mapping, since
    elements cannot be removed from arrays in Solidity.

--- contracts/Events.sol
+++ contracts/Events.sol
@@ -4,9 +4,24 @@ pragma solidity >=0.8.9 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Events is Ownable {
-    address[] public charities;
+    mapping(address => bool) public charities;
+
+    modifier isOwnerOrCharity(address charityAddress) {
+        require(
+            msg.sender == owner() || msg.sender == charityAddress,
+            "unauthorized"
+        );
+        _;
+    }
 
     function registerCharity(address charityAddress) public onlyOwner {
-        charities.push(charityAddress);
+        charities[charityAddress] = true;
+    }
+
+    function removeCharity(address charityAddress)
+        public
+        isOwnerOrCharity(charityAddress)
+    {
+        delete charities[charityAddress];
     }
 }
--- test/events.js
+++ test/events.js
@@ -17,14 +17,35 @@ contract('Events', async accounts => {
 
     await instance.registerCharity(charity1);
     assert.equal(
-      await instance.charities([0]),
-      charity1,
+      await instance.charities(charity1),
+      true,
       'the owner cannot register charities'
     )
 
     await expectRevert(
       instance.registerCharity.call(charity1, { from: charity1 }),
       'Ownable: caller is not the owner',
+    );
+  });
+
+  it('allows the owner or the charity itself to remove charities', async () => {
+    await instance.removeCharity(charity1);
+    assert.equal(
+      await instance.charities(charity1),
+      false,
+      'the owner cannot remove a charity'
+    );
+
+    await instance.removeCharity.call(charity1, { from: charity1 });
+    assert.equal(
+      await instance.charities(charity1),
+      false,
+      'a charity cannot remove itself'
+    );
+
+    await expectRevert(
+      instance.removeCharity.call(charity1, { from: charity2 }),
+      'unauthorized',
     );
   });
 });
