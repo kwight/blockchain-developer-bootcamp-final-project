87f5f9928083702865e3d7252b9c2472ae13fc8c
Kirk Wight <kwight@kwight.ca>
2021-10-22 08:37:33 -0700

    Add helper functions for connecting to MetaMask and determining a connection.

--- src/scripts/wallet.js
+++ src/scripts/wallet.js
@@ -1,1 +1,8 @@
-export const isMetaMaskInstalled = () => typeof window.ethereum !== 'undefined';
+export const isMetaMaskInstalled = () => typeof ethereum !== 'undefined';
+
+export const isMetaMaskConnected = () => !!ethereum.selectedAddress;
+
+export const connectToMetaMask = async () => {
+    await ethereum.request({ method: 'eth_requestAccounts' });
+    return ethereum.selectedAddress;
+}
