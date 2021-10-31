369250e72638984ee42d600eb117f65962c9beaf
Kirk Wight <kwight@kwight.ca>
2021-10-31 09:44:22 -0700

    Wire together the "Register Charity" button and its action.

--- src/scripts/owner.js
+++ src/scripts/owner.js
@@ -1,4 +1,4 @@
-import { addAccountsChangedListener, getConnectedAccount, isValidAddress } from './utils.js';
+import { addAccountsChangedListener, getConnectedAccount, isValidAddress, registerCharity } from './utils.js';
 
 const registerCharityForm = document.getElementById('register-charity');
 const registerCharityButton = document.getElementById('register-charity-button');
@@ -15,22 +15,28 @@ const init = async () => {
 
 const updateRegisterCharityButton = (account) => {
     if (!account || account.length == 0) {
-        registerCharityForm.removeEventListener('submit', registerCharity);
+        registerCharityForm.removeEventListener('submit', registerCharityListener);
         registerCharityButton.disabled = true;
     } else {
-        registerCharityForm.addEventListener('submit', registerCharity);
+        registerCharityForm.addEventListener('submit', registerCharityListener);
         registerCharityButton.disabled = false;
     }
 }
 
-const registerCharity = async (event) => {
+const registerCharityListener = async (event) => {
     event.preventDefault();
     const address = new FormData(registerCharityForm).get('address');
     if (!isValidAddress(address)) {
         alert('Address is invalid.');
         return;
     }
-    console.log(address);
+    try {
+        const result = await registerCharity(address);
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 
 init();
