4c90c576d1ad7b465d493edcf7d66193a630ff7f
Kirk Wight <kwight@kwight.ca>
2021-11-05 07:08:25 -0700

    Move all connection-related JS to `wallet.js`, and leave `fundraisers.js` for read-only operations.

--- src/scripts/fundraisers.js
+++ src/scripts/fundraisers.js
@@ -1,56 +1,10 @@
-import { connectToMetaMask, getCharities, getConnectedAccount, isMetaMaskInstalled } from './utils.js';
-import { ethers } from './ethers-5.1.esm.min.js';
+import { getCharities } from './utils.js';
 
 window.fundraisers = {
-    connectToMetaMask,
-    ethers,
     getCharities,
-    getConnectedAccount,
-    isMetaMaskInstalled,
 };
 
-const connectButton = document.getElementById('connect-button');
-const connectSection = document.getElementById('connect-section');
-
 const init = async () => {
-    if (!isMetaMaskInstalled()) {
-        renderInstallMetaMask();
-        return;
-    }
-
-    try {
-        const account = await getConnectedAccount();
-        renderAccountData(account);
-        ethereum.on('accountsChanged', renderAccountData);
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-const renderAccountData = (account) => {
-    if (!account || account.length == 0) {
-        connectButton.addEventListener('click', connectToMetaMask);
-        const accountData = document.getElementById('current-account');
-        if (accountData) {
-            accountData.remove();
-        }
-        connectButton.disabled = false;
-    } else {
-        const existingAccount = document.querySelector('#connect-section #current-account');
-        if (existingAccount) {
-            existingAccount.remove();
-        }
-        const selectedAccount = document.getElementById('selected-account').content;
-        connectSection.appendChild(selectedAccount.cloneNode(true));
-        const accountAddress = document.getElementById('account-address');
-        accountAddress.innerHTML = account;
-        connectButton.disabled = true;
-    }
-}
-
-const renderInstallMetaMask = () => {
-    const installMetaMask = document.getElementById('install-metamask').content;
-    connectSection.appendChild(installMetaMask.cloneNode(true));
 }
 
 init();
---
+++ src/scripts/wallet.js
@@ -0,0 +1,53 @@
+import { connectToMetaMask, getConnectedAccount, isMetaMaskInstalled } from './utils.js';
+
+window.fundraisers = {
+    connectToMetaMask,
+    getConnectedAccount,
+    isMetaMaskInstalled,
+};
+
+const connectButton = document.getElementById('connect-button');
+const connectSection = document.getElementById('connect-section');
+
+const init = async () => {
+    if (!isMetaMaskInstalled()) {
+        renderInstallMetaMask();
+        return;
+    }
+
+    try {
+        const account = await getConnectedAccount();
+        renderAccountData(account);
+        ethereum.on('accountsChanged', renderAccountData);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const renderAccountData = (account) => {
+    if (!account || account.length == 0) {
+        connectButton.addEventListener('click', connectToMetaMask);
+        const accountData = document.getElementById('current-account');
+        if (accountData) {
+            accountData.remove();
+        }
+        connectButton.disabled = false;
+    } else {
+        const existingAccount = document.querySelector('#connect-section #current-account');
+        if (existingAccount) {
+            existingAccount.remove();
+        }
+        const selectedAccount = document.getElementById('selected-account').content;
+        connectSection.appendChild(selectedAccount.cloneNode(true));
+        const accountAddress = document.getElementById('account-address');
+        accountAddress.innerHTML = account;
+        connectButton.disabled = true;
+    }
+}
+
+const renderInstallMetaMask = () => {
+    const installMetaMask = document.getElementById('install-metamask').content;
+    connectSection.appendChild(installMetaMask.cloneNode(true));
+}
+
+init();
