41ca9c1c9d4f565331eab2777ae5c05e792f3877
Kirk Wight <kwight@kwight.ca>
2021-11-07 07:35:07 -0800

    Add wallet handling to the charities view.

---
+++ src/scripts/charity.js
@@ -0,0 +1,79 @@
+import { addAccountsChangedListener, getConnectedAccount, isMetaMaskInstalled, isValidAddress } from './utils.js';
+
+const registerEventForm = document.getElementById('register-event');
+const registerEventButton = document.getElementById('register-event-button');
+const registeredCharities = document.getElementById('registered-events');
+const registeredEvent = document.getElementById('registered-event').content;
+
+const init = async () => {
+    if (!isMetaMaskInstalled()) {
+        return;
+    }
+
+    try {
+        const account = await getConnectedAccount();
+        updateRegisterEventButton(account);
+        addAccountsChangedListener(updateRegisterEventButton);
+        // window.fundraisers.provider.on('block', renderCharities);
+        // Object.assign(window.fundraisers, { registerEvent, removeEvent });
+        // renderEvents();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const updateRegisterEventButton = (account) => {
+    if (!account || account.length == 0) {
+        registerEventForm.removeEventListener('submit', registerEventListener);
+        registerEventButton.disabled = true;
+    } else {
+        registerEventForm.addEventListener('submit', registerEventListener);
+        registerEventButton.disabled = false;
+    }
+}
+
+const registerEventListener = async (event) => {
+    event.preventDefault();
+    const title = new FormData(registerEventForm).get('title');
+    const date = new FormData(registerEventForm).get('date');
+    try {
+        await registerEvent(title, date);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const registerEvent = async (title, date) => {
+    try {
+        const { contract, signer } = window.fundraisers;
+        const writableContract = contract.connect(signer);
+        await writableContract.registerEvent(title, date);
+        // renderEvents();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// const removeEvent = async (address) => {
+//     try {
+//         const { contract, signer } = window.fundraisers;
+//         const writableContract = contract.connect(signer);
+//         await writableContract.removeEvent(address);
+//         renderEvents();
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// const renderEvents = async () => {
+//     const events = await window.fundraisers.getCharities();
+//     registeredCharities.innerHTML = '';
+//     events.forEach(address => {
+//         const event = registeredEvent.cloneNode(true);
+//         event.querySelector('.event-address').innerText = address;
+//         event.querySelector('.remove-event').addEventListener('click', () => removeEvent(address));
+//         registeredCharities.appendChild(event);
+//     });
+// }
+
+init();
