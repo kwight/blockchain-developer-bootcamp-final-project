f5212b861247e878132aa3451bac44228e607853
Kirk Wight <kwight@kwight.ca>
2021-11-12 09:21:15 -0800

    Render events on the participant view.

---
+++ src/scripts/participant.js
@@ -0,0 +1,96 @@
+import { renderEvents } from './events.js';
+import { addAccountsChangedListener, getConnectedAccount, isMetaMaskInstalled } from './wallet.js';
+
+// const registerEventForm = document.getElementById('register-event');
+// const registerEventButton = document.getElementById('register-event-button');
+// const registeredEvents = document.getElementById('registered-events');
+// const registeredEvent = document.getElementById('registered-event').content;
+
+const init = async () => {
+    if (!isMetaMaskInstalled()) {
+        return;
+    }
+
+    try {
+        // const account = await getConnectedAccount();
+        // updateRegisterEventButton(account);
+        // addAccountsChangedListener(updateRegisterEventButton);
+        // addAccountsChangedListener(renderParticipantEvents);
+        // window.fundraisers.provider.on('block', renderParticipantEvents);
+        // Object.assign(window.fundraisers, { registerEvent, cancelEvent });
+        renderParticipantEvents();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// const updateRegisterEventButton = (account) => {
+//     if (!account || account.length == 0) {
+//         registerEventForm.removeEventListener('submit', registerEventListener);
+//         registerEventButton.disabled = true;
+//     } else {
+//         registerEventForm.addEventListener('submit', registerEventListener);
+//         registerEventButton.disabled = false;
+//     }
+// }
+
+// const registerEventListener = async (event) => {
+//     event.preventDefault();
+//     const title = new FormData(registerEventForm).get('title');
+//     const date = new FormData(registerEventForm).get('date');
+//     if (!title || !date) {
+//         alert('Both fields are required.');
+//         return;
+//     }
+//     const timestamp = Math.floor(new Date(date).getTime() / 1000);
+//     try {
+//         await registerEvent(title, timestamp);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+const registerForEvent = async (index) => {
+    // try {
+    //     const { contract, signer } = window.fundraisers;
+    //     const writableContract = contract.connect(signer);
+    //     await writableContract.registerEvent(title, date);
+    //     renderParticipantEvents();
+    // } catch (error) {
+    //     console.log(error);
+    // }
+}
+
+// const cancelEvent = async (index) => {
+//     try {
+//         const { contract, signer } = window.fundraisers;
+//         const writableContract = contract.connect(signer);
+//         await writableContract.cancelEvent(index);
+//         renderParticipantEvents();
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+const renderParticipantEvents = async () => {
+    await renderEvents();
+    const account = await getConnectedAccount();
+    const events = document.querySelectorAll('.event');
+    if (account) {
+        events.forEach(event => {
+            const index = event.id.match(/(?<=event-).+/)[0];
+            const status = event.querySelector('.event-status').innerText;
+            const button = document.createElement('button');
+            button.classList.add('register-for-event');
+            button.innerText = 'Register';
+            button.disabled = true;
+            if ('active' == status) {
+                button.disabled = false;
+                button.addEventListener('click', () => registerForEvent(index));
+            }
+            event.appendChild(button);
+        });
+    }
+}
+
+init();
