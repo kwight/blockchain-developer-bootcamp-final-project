4b061493ff4ee97e9fc415987011e0ee5e566794
Kirk Wight <kwight@kwight.ca>
2021-11-14 08:01:02 -0800

    Re-render events and charities on blockchain changes for the home view.

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,5 +1,10 @@
+import { contract } from './fundraisers.js';
 import { renderCharities } from './charities.js';
 import { renderEvents } from './events.js';
 
+contract.on('CharityRegistered', renderCharities);
+contract.on('CharityRemoved', renderCharities);
+contract.on('EventRegistered', renderEvents);
+contract.on('EventCancelled', renderEvents);
 renderCharities();
 renderEvents();
