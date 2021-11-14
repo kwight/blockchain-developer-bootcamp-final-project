b28210185339f28af36a54fefc08d098c003bb94
Kirk Wight <kwight@kwight.ca>
2021-11-14 13:59:34 -0800

    Add basic rendering of particiapnts in the donation form.

--- src/scripts/doner.js
+++ src/scripts/doner.js
@@ -1,7 +1,31 @@
-import { renderParticipants } from './participants.js';
+import { getParticipants } from './participants.js';
+import { isMetaMaskInstalled } from './wallet.js';
+
+const participantRadioButton = document.getElementById('participant-radio-button');
+const donationForm = document.getElementById('donation-form');
 
 const init = () => {
-    renderParticipants();
+    renderParticipantRadioButtons();
+    if (!isMetaMaskInstalled()) {
+        return;
+    }
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+const renderParticipantRadioButtons = async () => {
+    const participants = await getParticipants();
+    participants.forEach((address, index) => {
+        const participant = participantRadioButton.content.cloneNode(true);
+        const label = participant.querySelector('label');
+        participant.querySelector('input').id = `participant-${index}`;
+        label.setAttribute('for', `participant-${index}`);
+        label.innerText = address;
+        donationForm.prepend(participant);
+    });
 }
 
 init();
