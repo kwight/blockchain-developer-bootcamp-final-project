aad22682b17f15c5759ed23d03bca386837c4e37
Kirk Wight <kwight@kwight.ca>
2021-11-16 06:11:38 -0800

    Show participants on the home view.

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,6 +1,7 @@
 import { contract } from './fundraisers.js';
 import { renderCharities } from './charities.js';
 import { renderEvents } from './events.js';
+import { renderParticipants } from './participants.js';
 
 contract.on('CharityRegistered', renderCharities);
 contract.on('CharityRemoved', renderCharities);
@@ -8,3 +9,4 @@ contract.on('EventRegistered', renderEvents);
 contract.on('EventCancelled', renderEvents);
 renderCharities();
 renderEvents();
+renderParticipants();
