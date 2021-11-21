3565df5a8324dad18e4df785cf400100115e0659
Kirk Wight <kwight@kwight.ca>
2021-11-21 08:51:34 -0800

    Update charity registration with the new name parameter.

--- src/scripts/fundraisers.js
+++ src/scripts/fundraisers.js
@@ -3,7 +3,7 @@ import { ethers } from './ethers-5.1.esm.min.js';
 const contractAddress = '0x5b00D792A7A6ceFc7cFB6ee39DE6d4FD916B2402';
 const abi = [
     'function getCharities() public view returns (address[] memory)',
-    'function registerCharity(address charityAddress) public',
+    'function registerCharity(address charityAddress, string name) public',
     'function removeCharity(address charityAddress) public',
     'function getPrograms() view returns (tuple(string title, uint8 status, address charity, uint256 timestamp)[])',
     'function registerProgram(string memory title, uint256 timestamp)',
