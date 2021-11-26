ce10b7ac63fd0a9843f682dc5f91873752030a05
Kirk Wight <kwight@kwight.ca>
2021-11-26 13:20:14 -0800

    Add network details to the connection section.

--- src/scripts/wallet.js
+++ src/scripts/wallet.js
@@ -15,27 +15,35 @@ const init = async () => {
         const account = await getConnectedAccount();
         renderAccountData(account);
         window.ethereum.on('accountsChanged', renderAccountData);
+        window.ethereum.on('chainChanged', renderAccountData);
     } catch (error) {
         console.log(error);
     }
 }
 
-const renderAccountData = (account) => {
+const renderAccountData = async () => {
+    const account = await getConnectedAccount();
     if (!account || account.length == 0) {
         connectButton.addEventListener('click', connectToMetaMask);
         const accountData = document.getElementById('current-account');
+        const networkData = document.getElementById('current-network');
         if (accountData) {
             accountData.remove();
+            networkData.remove();
         }
         connectButton.disabled = false;
     } else {
         const existingAccount = document.querySelector('#connect-section #current-account');
+        const existingNetwork = document.querySelector('#connect-section #current-network');
         if (existingAccount) {
             existingAccount.remove();
+            existingNetwork.remove();
         }
         connectSection.appendChild(selectedAccount.cloneNode(true));
         const accountAddress = document.getElementById('account-address');
+        const network = document.getElementById('network');
         accountAddress.innerHTML = account;
+        network.innerText = getNetworkName();
         connectButton.disabled = true;
     }
 }
@@ -50,6 +58,23 @@ export const connectToMetaMask = async () => {
 export const getConnectedAccount = async () => {
     const accounts = await window.ethereum.request({ method: 'eth_accounts' });
     return (accounts.length > 0) ? window.ethereum.selectedAddress : false;
+}
+
+export const getNetworkName = () => {
+    switch (window.ethereum.chainId) {
+        case '0x1':
+            return 'Mainnet';
+        case '0x3':
+            return 'Ropsten';
+        case '0x4':
+            return 'Rinkeby';
+        case '0x5':
+            return 'Görli';
+        case '0x2a':
+            return 'Kovan';
+        default:
+            return 'Localhost or custom';
+    }
 }
 
 export const addAccountsChangedListener = (listener) => window.ethereum.on('accountsChanged', listener);
