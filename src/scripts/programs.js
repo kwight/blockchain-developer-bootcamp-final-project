b56e87ad364cb8fc825283bb763ba7f77736f2e4
Kirk Wight <kwight@kwight.ca>
2021-11-26 22:40:08 -0800

    Render programs as a table on the home view.

--- src/scripts/programs.js
+++ src/scripts/programs.js
@@ -17,12 +17,15 @@ export const renderPrograms = async () => {
     const loading = spinner.content.cloneNode(true);
     registeredPrograms.replaceChildren(loading);
     const programs = await getPrograms();
-    registeredPrograms.innerHTML = '';
-    programs.forEach((programData, index) => {
+    registeredPrograms.innerHTML = '<thead><tr><th>Title</th><th>Charity</th><th>Status</th></tr></thead><tbody></tbody>';
+    const tableBody = registeredPrograms.querySelector('tbody');
+    for (const [index, programData] of programs.entries()) {
+        const charityData = await contract.getCharity(programData.charity);
         const program = registeredProgram.content.cloneNode(true);
         program.querySelector('.program').id = `program-${index}`;
         program.querySelector('.program-title').innerText = programData.title;
+        program.querySelector('.program-charity').innerText = charityData.name;
         program.querySelector('.program-status').innerText = status[programData.status];
-        registeredPrograms.appendChild(program);
-    });
+        tableBody.appendChild(program);
+    }
 }
