45fae897e26bf0be1506b7aba24d5d30731cd490
Kirk Wight <kwight@kwight.ca>
2021-11-28 14:42:29 -0800

    Add NatSpec comments to the contract for all public entities.

--- contracts/Fundraisers.sol
+++ contracts/Fundraisers.sol
@@ -3,6 +3,9 @@ pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/// @title Fundraiser donations
+/// @author kwight
+/// @notice This contract allows register charities to receive donations for campaigns.
 contract Fundraisers is Ownable {
     mapping(address => Charity) private charityRegistry;
     address[] private charities;
@@ -32,11 +35,35 @@ contract Fundraisers is Ownable {
         uint256 amount;
     }
 
+    /// @notice Emit an event when a charity is registered.
+    /// @param charityAddress Address of the registered charity.
+    /// @param name Name of the charity.
     event CharityRegistered(address charityAddress, string name);
+
+    /// @notice Emit an event when a charity is removed by the owner.
+    /// @param charityAddress Address of the removed charity.
     event CharityRemoved(address charityAddress);
+
+    /// @notice Emit an event when a charity registers a program.
+    /// @param programId Index in the programs array.
+    /// @param charityAddress Address of the charity doing the registering.
     event ProgramRegistered(uint256 programId, address charityAddress);
+
+    /// @notice Emit an event when a charity cancels a program.
+    /// @param programId Index in the programs array.
+    /// @param charityAddress Address of the charity doing the cancelling.
     event ProgramCancelled(uint256 programId, address charityAddress);
+
+    /// @notice Emit an event when a charity marks a program as completed.
+    /// @param programId Index in the programs array.
+    /// @param charityAddress Address of the charity marking the completion.
     event ProgramCompleted(uint256 programId, address charityAddress);
+
+    /// @notice Emit an event when a donation is received.
+    /// @param amount Amount of the donation (in wei).
+    /// @param charityAddress Address of the charity receiving the funds.
+    /// @param programId Index in the programs array.
+    /// @param doner Address of the doner.
     event DonationReceived(
         uint256 amount,
         address charityAddress,
@@ -57,6 +84,9 @@ contract Fundraisers is Ownable {
         _;
     }
 
+    /// @notice Verify a given address is a registered charity.
+    /// @param charityAddress Address being verified.
+    /// @return True if registered, false otherwise.
     function isRegisteredCharity(address charityAddress)
         public
         view
@@ -67,6 +97,9 @@ contract Fundraisers is Ownable {
             charityAddress);
     }
 
+    /// @notice Register a charity.
+    /// @param charityAddress Address of the charity to be registered.
+    /// @param name Name of the charity.
     function registerCharity(address charityAddress, string memory name)
         public
         onlyOwner
@@ -81,6 +114,8 @@ contract Fundraisers is Ownable {
         emit CharityRegistered(charityAddress, name);
     }
 
+    /// @notice Remove a charity.
+    /// @param charityAddress Address of the charity to be removed.
     function removeCharity(address charityAddress)
         public
         isOwnerOrCharity(charityAddress)
@@ -97,6 +132,9 @@ contract Fundraisers is Ownable {
         emit CharityRemoved(charityAddress);
     }
 
+    /// @notice Get stored data for the given charity.
+    /// @param charityAddress Address of the charity requested.
+    /// @return Charity struct instance.
     function getCharity(address charityAddress)
         public
         view
@@ -109,14 +147,20 @@ contract Fundraisers is Ownable {
         return charityRegistry[charityAddress];
     }
 
+    /// @notice Get data for all registered charities.
+    /// @return Array of Charity struct instances.
     function getCharities() public view returns (address[] memory) {
         return charities;
     }
 
+    /// @notice Get data for all registered programs.
+    /// @return Array of Program struct instances.
     function getPrograms() public view returns (Program[] memory) {
         return programs;
     }
 
+    /// @notice Register a program.
+    /// @param title The name of the program.
     function registerProgram(string memory title) public onlyCharity {
         programs.push(
             Program({
@@ -128,6 +172,8 @@ contract Fundraisers is Ownable {
         emit ProgramRegistered(programs.length - 1, msg.sender);
     }
 
+    /// @notice Cancel an active program.
+    /// @param programId Index of the program to be cancelled.
     function cancelProgram(uint256 programId) public onlyCharity {
         require(programs.length > programId, "program does not exist");
         require(programs[programId].charity == msg.sender, "unauthorized");
@@ -139,6 +185,8 @@ contract Fundraisers is Ownable {
         emit ProgramCancelled(programId, msg.sender);
     }
 
+    /// @notice Mark an active program as complete.
+    /// @param programId Index of the program to be marked complete.
     function completeProgram(uint256 programId) public onlyCharity {
         require(programs.length > programId, "program does not exist");
         require(programs[programId].charity == msg.sender, "unauthorized");
@@ -150,6 +198,8 @@ contract Fundraisers is Ownable {
         emit ProgramCompleted(programId, msg.sender);
     }
 
+    /// @notice Donate ether to a charity's program.
+    /// @param programId Index of the program "receiving" the donation.
     function donate(uint256 programId) public payable {
         require(programs.length > programId, "program does not exist");
         require(
@@ -174,6 +224,8 @@ contract Fundraisers is Ownable {
         );
     }
 
+    /// @notice Get data for all donations.
+    /// @return Array of Donation struct instances.
     function getDonations() public view returns (Donation[] memory) {
         return donations;
     }
