ff4f10fd48ad5d8be10b4cfee7adb8aebb80b93f
Kirk Wight <kwight@kwight.ca>
2021-11-29 06:56:13 -0800

    Catch any errors in `home.js`.

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,13 +1,17 @@
-import { contract } from './fundraisers.js';
+import { contract, renderNotice } from './fundraisers.js';
 import { renderCharities } from './charities.js';
 import { renderPrograms } from './programs.js';
 import { renderDonations } from './donations.js';
 
-contract.on('CharityRegistered', renderCharities);
-contract.on('CharityRemoved', renderCharities);
-contract.on('ProgramRegistered', renderPrograms);
-contract.on('ProgramCancelled', renderPrograms);
-contract.on('DonationReceived', renderDonations);
-renderCharities();
-renderPrograms();
-renderDonations();
+try {
+    contract.on('CharityRegistered', renderCharities);
+    contract.on('CharityRemoved', renderCharities);
+    contract.on('ProgramRegistered', renderPrograms);
+    contract.on('ProgramCancelled', renderPrograms);
+    contract.on('DonationReceived', renderDonations);
+    renderCharities();
+    renderPrograms();
+    renderDonations();
+} catch (error) {
+    renderNotice('error', error.data.message);
+}
