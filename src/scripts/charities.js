d84b38577e20c907b264031ce30b6b5784e1b565
Kirk Wight <kwight@kwight.ca>
2021-11-29 07:03:26 -0800

    Catch any errors in `charities.js`.

--- src/scripts/charities.js
+++ src/scripts/charities.js
@@ -1,26 +1,34 @@
-import { contract, getAddressMarkup } from "./fundraisers.js";
+import { contract, getAddressMarkup, renderNotice } from "./fundraisers.js";
 
 const spinner = document.getElementById('spinner');
 const registeredCharities = document.getElementById('registered-charities');
 const registeredCharity = document.getElementById('registered-charity');
 
 export const getCharities = async () => {
-    return await contract.getCharities();
+    try {
+        return await contract.getCharities();
+    } catch (error) {
+        renderNotice('error', error.data.message);
+    }
 }
 
 export const renderCharities = async () => {
-    const loading = spinner.content.cloneNode(true);
-    registeredCharities.replaceChildren(loading);
-    const charities = await getCharities();
-    registeredCharities.innerHTML = '<thead><tr><th>Name</th><th class="address-column">Address</th></tr></thead><tbody></tbody>';
-    const tableBody = registeredCharities.querySelector('tbody');
-    for (const address of charities) {
-        const charityData = await contract.getCharity(address);
-        const addressMarkup = await getAddressMarkup(address);
-        const charity = registeredCharity.content.cloneNode(true);
-        charity.querySelector('.charity').id = `charity-${address}`;
-        charity.querySelector('.charity-name').innerText = charityData.name;
-        charity.querySelector('.charity-address').replaceChildren(addressMarkup);
-        tableBody.appendChild(charity);
+    try {
+        const loading = spinner.content.cloneNode(true);
+        registeredCharities.replaceChildren(loading);
+        const charities = await getCharities();
+        registeredCharities.innerHTML = '<thead><tr><th>Name</th><th class="address-column">Address</th></tr></thead><tbody></tbody>';
+        const tableBody = registeredCharities.querySelector('tbody');
+        for (const address of charities) {
+            const charityData = await contract.getCharity(address);
+            const addressMarkup = await getAddressMarkup(address);
+            const charity = registeredCharity.content.cloneNode(true);
+            charity.querySelector('.charity').id = `charity-${address}`;
+            charity.querySelector('.charity-name').innerText = charityData.name;
+            charity.querySelector('.charity-address').replaceChildren(addressMarkup);
+            tableBody.appendChild(charity);
+        }
+    } catch (error) {
+        renderNotice('error', error.data.message);
     }
 }
